f0dfbca3a49678d59a19cba968b5857bf7705bf8
aemogie <theaemogie@gmail.com>
2025-08-10 13:51:43 +0530

    chore: add posix man pages to environment

--- manifest.scm
+++ manifest.scm
@@ -1,5 +1,6 @@
 (use-modules ((guix profiles) #:select (packages->manifest))
 	     ((gnu packages commencement) #:select (gcc-toolchain))
-	     ((gnu packages aspell) #:select (aspell)))
+	     ((gnu packages man) #:select (man-db man-pages-posix)))
 
-(packages->manifest (list gcc-toolchain))
+(packages->manifest (list gcc-toolchain
+			  man-db man-pages-posix))
